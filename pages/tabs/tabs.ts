eccbb6c9c46eeed9d1ac5d1b079d65ac9dd20776
nStrazzer <norman.strazzer.2002@calvino.edu.it>
2021-04-20 13:34:53 +0200

    2 files modified, 2 added

--- pages/tabs/tabs.ts
+++ pages/tabs/tabs.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { AboutPage } from "../about/about";
+import { ContactPage } from "../contact/contact";
+import { StorePage } from "../store/store";
+import { HomePage } from "../home/home";
+
 
 @Component({
-  templateUrl: 'tabs.html'
+  templateUrl: "tabs.html"
 })
 export class TabsPage {
-
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = StorePage;
 
-  constructor() {
-
-  }
+  constructor() {}
 }
